3d6e6a854cfe2e7c4e2e4179bb12f1e016f59a39
suyashpanwar <suyashp124@gmail.com>
2024-04-05 19:10:12 +0530

    course purchase backend in express with JWT authentication

---
+++ courseBackendJWTauth.js
@@ -0,0 +1,156 @@
+const express = require('express');
+const jwt = require('jsonwebtoken')
+const app = express();
+
+app.use(express.json());
+
+let ADMINS = [];
+let USERS = [];
+let COURSES = [];
+
+const secretKey = "secret_key"
+
+const generateJwt = (user) =>{
+    const payload = {username : user.username};
+    return jwt.sign(payload, secretKey, {expiresIn : '1h'})
+}
+
+const authenticateJwt = (req,res,next) => {
+    const authHeader = req.headers.authorization;
+
+    if(authHeader){
+        const token = authHeader.split(' ')[1];
+
+        jwt.verify(token, secretKey, (err,user)=>{
+            if(err){
+                res.sendStatus(403)
+            }
+
+            req.user = user
+            next();
+        })
+    }else{
+        res.sendStatus(401)
+    }
+}
+// Admin routes
+app.post('/admin/signup', (req, res) => {
+  // logic to sign up admin
+  const admin = req.body
+  const existingAdmin = ADMINS.find(a=>a.username===admin.username)
+  if(existingAdmin){
+    res.status(403).json({message:'admin already exists'})
+  }
+  else{
+    ADMINS.push(admin)
+    const token = generateJwt(admin)
+    res.json({message : 'admin created successfully', token})
+  }
+});
+
+app.post('/admin/login', (req, res) => {
+  // logic to log in admin
+  const {username, password} = req.headers 
+  const admin = ADMINS.find(a=>a.username===username && a.password===password)
+
+  if(admin){
+    const token = generateJwt(admin)
+    res.json({message : 'logged in successfully', token})
+  }
+  else{
+    res.status(403).json({message : 'admin authentication failed'})
+  }
+});
+
+app.post('/admin/courses',authenticateJwt, (req, res) => {
+  // logic to create a course
+  const course = req.body
+  course.id = Date.now()
+
+  COURSES.push(course);
+  res.json({message : 'course created successfully', courseID : course.id})
+});
+
+app.put('/admin/courses/:courseId',authenticateJwt,  (req, res) => {
+  // logic to edit a course
+  const courseId = parseInt(req.params.courseId)
+  const course = COURSES.find(c=>c.id === courseId)
+  if (course) {
+    Object.assign(course, req.body);
+    res.json({ message: 'Course updated successfully' });
+  } else {
+    res.status(404).json({ message: 'Course not found' });
+  }
+});
+
+app.get('/admin/courses', authenticateJwt, (req, res) => {
+  // logic to get all courses
+  res.json({course : COURSES})
+});
+
+// User routes
+app.post('/users/signup', (req, res) => {
+  // logic to sign up user
+  const user = req.body
+  const exisitingUser = USERS.find(u=>u.username===user.username)
+  if(exisitingUser){
+    res.status(403).json({message: 'user already exists'})
+  }
+  else{
+    USERS.push(user)
+    const token = generateJwt(user)
+    res.json({message: 'user created successfully', token})
+  }
+});
+
+app.post('/users/login',  (req, res) => {
+  // logic to log in user
+  const {username, password} = req.headers
+  const user = USERS.find(u=>u.username===username && u.password===password)
+  if(user){
+    const token = generateJwt(user)
+    res.json({message : 'logged in successfully', token})
+  }
+  else{
+    res.status(403).json({message: 'user authentication failed'})
+  }
+});
+
+app.get('/users/courses', authenticateJwt, (req, res) => {
+  // logic to list all courses
+  res.json({ courses: COURSES });
+});
+
+app.post('/users/courses/:courseId', authenticateJwt, (req, res) => {
+  // logic to purchase a course
+  const courseId = parseInt(req.params.courseId);
+  const course = COURSES.find(c => c.id === courseId);
+  if (course) {
+    const user = USERS.find(u => u.username === req.user.username);
+    if (user) {
+      if (!user.purchasedCourses) {
+        user.purchasedCourses = [];
+      }
+      user.purchasedCourses.push(course);
+      res.json({ message: 'Course purchased successfully' });
+    } else {
+      res.status(403).json({ message: 'User not found' });
+    }
+  } else {
+    res.status(404).json({ message: 'Course not found' });
+  }
+});
+
+app.get('/users/purchasedCourses',authenticateJwt, (req, res) => {
+  // logic to view purchased courses
+  const user = USERS.find(u => u.username === req.user.username);
+  if (user && user.purchasedCourses) {
+    res.json({ purchasedCourses: user.purchasedCourses });
+  } else {
+    res.status(404).json({ message: 'No courses purchased' });
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Server is listening on port 3000');
+});
